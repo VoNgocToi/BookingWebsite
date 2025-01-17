92da15a63e93cb7c02ed1a079a9d7ed2a18893c9
VoNgocToi <122598949+VoNgocToi@users.noreply.github.com>
2025-01-17 11:59:34 +0700

    fhsh

--- src/app/service/statistics.service.ts
+++ src/app/service/statistics.service.ts
@@ -10,9 +10,15 @@ export class StatisticsService {
 
   private apiUrl = 'http://localhost:8080/api/statistics'; // URL API backend
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = this.authService.getToken();  // Lấy token từ AuthService
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
 
   getStatisticsOverview(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/overview`);
+    const headers = this.getAuthHeaders(); 
+    return this.http.get(`${this.apiUrl}/overview`, { headers });
   }
 }
